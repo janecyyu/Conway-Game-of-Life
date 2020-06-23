169e73537d587fbda32b26d070eceb5e9606f90f
janecyyu <janecyyu@gmail.com>
2020-06-23 13:18:51 -0700

    rules ok

--- gameOfLife/src/App.js
+++ gameOfLife/src/App.js
@@ -24,50 +24,62 @@ function App() {
               <div className="pre preset3"></div>
               <div className="pre preset4"></div>
             </div>
+            <div className="presetButtons">
+              <button className="preBtn">Preset 1</button>
+              <button className="preBtn">Preset 2</button>
+              <button className="preBtn">Preset 3</button>
+              <button className="preBtn">Preset 4</button>
+            </div>
           </div>
         </div>
-        <div className="rules">
-          <h2 className="ruleTitle">Rules:</h2>
-          <ul>
-            <li>
-              Any live cell with fewer than two live neighbours dies, as if by
-              underpopulation.
-            </li>
-            <li>
-              Any live cell with two or three live neighbours lives on to the
-              next generation.
-            </li>
-            <li>
-              Any live cell with more than three live neighbours dies, as if by
-              overpopulation.
-            </li>
-            <li>
-              Any dead cell with exactly three live neighbours becomes a live
-              cell, as if by reproduction.
-            </li>
-          </ul>
+        <div className="rulesAndAbout">
+          <div className="rules">
+            <h2 className="ruleTitle">Rules:</h2>
+            <div className="rulesContent">
+              <ul>
+                <li>
+                  Any live cell with fewer than two live neighbours dies, as if
+                  by underpopulation.
+                </li>
+                <li>
+                  Any live cell with two or three live neighbours lives on to
+                  the next generation.
+                </li>
+                <li>
+                  Any live cell with more than three live neighbours dies, as if
+                  by overpopulation.
+                </li>
+                <li>
+                  Any dead cell with exactly three live neighbours becomes a
+                  live cell, as if by reproduction.
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <h2 className="about">About</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+              pulvinar turpis sed ipsum porta eleifend. Phasellus ornare in
+              metus et convallis. Quisque eu laoreet urna, quis pellentesque
+              ipsum. Vestibulum rhoncus, sapien sit amet pellentesque finibus,
+              orci quam lobortis purus, quis commodo odio elit eu neque. Aenean
+              finibus elit ac molestie iaculis. Nullam ac sapien cursus, tempus
+              augue at, iaculis nulla. Etiam a nisi varius, placerat sapien sed,
+              finibus libero.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+              pulvinar turpis sed ipsum porta eleifend. Phasellus ornare in
+              metus et convallis. Quisque eu laoreet urna, quis pellentesque
+              ipsum. Vestibulum rhoncus, sapien sit amet pellentesque finibus,
+              orci quam lobortis purus, quis commodo odio elit eu neque. Aenean
+              finibus elit ac molestie iaculis. Nullam ac sapien cursus, tempus
+              augue at, iaculis nulla. Etiam a nisi varius, placerat sapien sed,
+              finibus libero.
+            </p>
+          </div>
         </div>
-      </div>
-      <div>
-        <h2 className="about">About</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-          pulvinar turpis sed ipsum porta eleifend. Phasellus ornare in metus et
-          convallis. Quisque eu laoreet urna, quis pellentesque ipsum.
-          Vestibulum rhoncus, sapien sit amet pellentesque finibus, orci quam
-          lobortis purus, quis commodo odio elit eu neque. Aenean finibus elit
-          ac molestie iaculis. Nullam ac sapien cursus, tempus augue at, iaculis
-          nulla. Etiam a nisi varius, placerat sapien sed, finibus libero.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-          pulvinar turpis sed ipsum porta eleifend. Phasellus ornare in metus et
-          convallis. Quisque eu laoreet urna, quis pellentesque ipsum.
-          Vestibulum rhoncus, sapien sit amet pellentesque finibus, orci quam
-          lobortis purus, quis commodo odio elit eu neque. Aenean finibus elit
-          ac molestie iaculis. Nullam ac sapien cursus, tempus augue at, iaculis
-          nulla. Etiam a nisi varius, placerat sapien sed, finibus libero.
-        </p>
       </div>
     </div>
   );
