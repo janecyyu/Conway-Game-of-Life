223a96878a2ebc0a33e77683bae20566204f03d1
janecyyu <janecyyu@gmail.com>
2020-06-22 20:02:53 -0700

    container ok

--- gameOfLife/src/App.js
+++ gameOfLife/src/App.js
@@ -4,28 +4,31 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Conway's Game of Life</h1>
-      <div className="rules">
-        Rules:
-        <ul>
-          <li>
-            Any live cell with fewer than two live neighbours dies, as if by
-            underpopulation.
-          </li>
-          <li>
-            Any live cell with two or three live neighbours lives on to the next
-            generation.
-          </li>
-          <li>
-            Any live cell with more than three live neighbours dies, as if by
-            overpopulation.
-          </li>
-          <li>
-            Any dead cell with exactly three live neighbours becomes a live
-            cell, as if by reproduction.
-          </li>
-        </ul>
+    <div className="app">
+      <h1 className="title">Conway's Game of Life</h1>
+      <div className="container">
+        <div className="display"></div>
+        <div className="rules">
+          Rules:
+          <ul>
+            <li>
+              Any live cell with fewer than two live neighbours dies, as if by
+              underpopulation.
+            </li>
+            <li>
+              Any live cell with two or three live neighbours lives on to the
+              next generation.
+            </li>
+            <li>
+              Any live cell with more than three live neighbours dies, as if by
+              overpopulation.
+            </li>
+            <li>
+              Any dead cell with exactly three live neighbours becomes a live
+              cell, as if by reproduction.
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
